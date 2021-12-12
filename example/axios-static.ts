f0db68dc8bd4417eafcbd020067cba3a7c62337b
Ilya Lisin <blacklacost@gmail.com>
2021-12-12 15:59:17 +0300

    jsdoc examples

---
+++ example/axios-static.ts
@@ -0,0 +1,34 @@
+import axios from 'axios'
+import { torInterceptor } from '../src'
+
+const socksPorts = [9050, 9052, 9054]
+
+const url = 'https://api.ipify.org/'
+torInterceptor(axios, socksPorts)
+torInterceptor
+
+async function originalIp() {
+  const response = await axios.get(url)
+  console.info('Original IP: ', response.data)
+}
+
+async function torIp() {
+  try {
+    const response = await axios.get(url, { headers: { tor: 'true' } })
+    console.info('Tor IP: ', response.data)
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      console.info(err.message)
+    }
+  }
+}
+
+async function main() {
+  await originalIp()
+  await torIp()
+  await torIp()
+  await torIp()
+  await torIp()
+}
+
+main()
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,48 @@
-import { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios'
+import {
+  AxiosInstance,
+  AxiosRequestConfig,
+  AxiosResponse,
+  AxiosStatic,
+} from 'axios'
 import { genSocksProxyAgents } from './genSocksProxyAgents'
 
+/**
+ * The torInterceptor
+ *
+ * Example using axios instance
+ *
+ * ```ts
+ * const url = 'https://api.ipify.org/'
+ * const api = axios.create({ baseURL: url })
+ * torInterceptor(api, [9050, 9052, 9054])
+ * api.get('/', { headers: { tor: 'true' } })
+ * ```
+ *
+ * Example using axios
+ *
+ * ```ts
+ * const url = 'https://api.ipify.org/'
+ * torInterceptor(axios, [9050, 9052, 9054])
+ * axios.get(url, { headers: { tor: 'true' } })
+ * ```
+ *
+ * @param axios Axios instance or axios
+ * @param socksPorts Socks ports opened by tor
+ */
 export const torInterceptor = (
-  instance: AxiosInstance,
+  axios: AxiosInstance | AxiosStatic,
   socksPorts: number[],
 ) => {
   const proxyAgents = genSocksProxyAgents(socksPorts)
 
-  instance.interceptors.request.use((request: AxiosRequestConfig) => {
+  axios.interceptors.request.use((request: AxiosRequestConfig) => {
     if (request.headers?.tor) {
       request.httpsAgent = proxyAgents.next().value
     }
     return request
   })
 
-  instance.interceptors.response.use((response: AxiosResponse) => {
+  axios.interceptors.response.use((response: AxiosResponse) => {
     if (response.config.httpsAgent) {
       delete response.config.httpsAgent
     }
