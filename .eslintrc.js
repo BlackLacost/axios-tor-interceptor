07e163ad615da1c7a1aeda120b29da27a4123c7d
Ilya Lisin <blacklacost@gmail.com>
2021-12-24 23:10:30 +0300

    ESLint import configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,13 +8,15 @@ module.exports = {
     tsconfigRootDir: __dirname, // tsconfigRootDir: __dirname, ???
     project: ['./tsconfig.json'],
   },
-  plugins: ['@typescript-eslint', 'jest'],
+  plugins: ['@typescript-eslint', 'jest', 'import'],
   extends: [
     'airbnb-typescript/base',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'prettier',
     'plugin:jest/recommended',
     'plugin:jest/style',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
   ],
   rules: {
     'jest/no-identical-title': 'error',
