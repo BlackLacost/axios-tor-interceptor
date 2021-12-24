87c373e94cf29d583f210f79f7c02021e84523b1
Ilya Lisin <blacklacost@gmail.com>
2021-12-24 17:50:46 +0300

    ESLint plugin jest configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,10 +8,15 @@ module.exports = {
     tsconfigRootDir: __dirname, // tsconfigRootDir: __dirname, ???
     project: ['./tsconfig.json'],
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'jest'],
   extends: [
     'airbnb-typescript/base',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'prettier',
+    'plugin:jest/recommended',
+    'plugin:jest/style',
   ],
+  rules: {
+    'jest/no-identical-title': 'error',
+  },
 }
